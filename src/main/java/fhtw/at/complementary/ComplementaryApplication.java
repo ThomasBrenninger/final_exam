7c8aa4bed8646d526c21c438da8bf1743fd13060
ic20b073 <ic20b073@technikum-wien.at>
2022-12-06 18:00:55 +0100

    #1 Initial commit

---
+++ src/main/java/fhtw/at/complementary/ComplementaryApplication.java
@@ -0,0 +1,13 @@
+package fhtw.at.complementary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplementaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplementaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fhtw/at/complementary/ComplementaryApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.at.complementary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplementaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
